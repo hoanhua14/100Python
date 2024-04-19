d8d86d21a68321b832a5bf592e352fe48bd9d299
hoanhua14 <huasohoan@gmail.com>
2024-04-18 17:21:48 -0700

    life in weeks

---
+++ life_in_weeks.py
@@ -0,0 +1,6 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+age_in_num = int(age)
+weeks_left = 4680 - age_in_num*52
+print(f'You have {weeks_left} weeks left.')
